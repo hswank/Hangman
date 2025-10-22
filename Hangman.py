53b114ad23174b5dbdab4aea945af7d843afe247
Heather Swank <hdamoth@gmail.com>
2025-10-21 22:33:21 -0400

    Fixed typos and indentation error

--- Hangman.py
+++ Hangman.py
@@ -38,11 +38,11 @@ HANGMAN_PICS = ['''
 words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferrett fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
 
 def getRandomWord(wordList):
-    wordIndex = random.randit(0, len(wordList) - 1)
+    wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
 
 def displayBoard(missedLetters, correctLetters, secretWord):
-    print(HANGMAN__PICS[len(missedLetters)])
+    print(HANGMAN_PICS[len(missedLetters)])
     print()
 
     print('Missed letters:', end=' ')
@@ -94,7 +94,8 @@ while True:
         correctLetters = correctLetters + guess
 
         foundAllLetters = True
-        for i in range(len(secretWord)):
+        
+        for i in range(len(secretWord)):
             if secretWord[i] not in correctLetters:
                 foundAllLetters = False
                 break
